647c08a15573f881170f3a62837f27358519cfaf
cdw108 <127534441+cdw108@users.noreply.github.com>
2025-10-27 14:51:45 -0500

    Update mouseHandler.py
    Added more explanatory comments for usage of feature

--- source/mouseHandler.py
+++ source/mouseHandler.py
@@ -107,16 +107,19 @@ def playAudioCoordinates(x, y, screenWidth, screenHeight, screenMinPos, detectBr
 def speakAudioCoordinates(x, y, screenMinPos):
 	"""Speaks the audio coordinates (see playAudioCoordinates for tone feedback of coords)
 	- Function variable waitTime: time between readouts of coordinates
+	- waitTime is time to wait after mouse move before reading coordinates
 	"""
 
 	waitTime = 0.1
-
 	sleep(waitTime)
 
 	# make coordinate values both positive and relative to (0, 0)
+	# taken from playAudioCoordinates in order to have normalized coordinate system
 	x = x - screenMinPos.x
 	y = y - screenMinPos.y
 
+	# Only call the coordinate message once the mouse has stopped moving
+	# Without check, queues multiple coordinate reads
 	if not mouseMoved:
 		ui.message(_(f"{x} x and {y} y"))
 
@@ -249,6 +252,7 @@ def executeMouseMoveEvent(x, y):
 			config.conf["mouse"]["audioCoordinates_blurFactor"],
 		)
 
+	# Checks the config file to determine if coordinates should be read
 	if config.conf["mouse"]["speakCoordinatesOnMouseMove"] and not oldMouseObject.sleepMode:
 		speakAudioCoordinates(x, y, minPos)
 
